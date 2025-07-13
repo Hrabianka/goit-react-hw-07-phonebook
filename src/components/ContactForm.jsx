85f9b2152257a23c85d1e914aae6858eff70ae4c
Anita Hrabia <34211073+Hrabianka@users.noreply.github.com>
2025-07-13 13:53:30 +0200

    Initial commit

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,85 @@
+import { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import styles from './ContactForm.module.css';
+
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [error, setError] = useState(''); 
+
+  const nameInputRef = useRef(null);
+  const numberInputRef = useRef(null);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    setError('');
+
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const phoneRegex = /^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+    if (!phoneRegex.test(number)) {
+      setError('The phone number is not in the right format.');
+      return;
+    }
+
+    onAddContact(name, number);
+
+    setName('');
+    setNumber('');
+    setError(''); 
+    nameInputRef.current.focus();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.formclass}>
+      <label className={styles.labelclass}>
+        Name
+        <input
+          className={styles.inputclass}
+          type="text"
+          name="name"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Mara, Stella Artois, Bill Gates."
+          required
+          value={name}
+          onChange={handleChange}
+          ref={nameInputRef}
+          placeholder="Ex: Jack Sparrow"
+        />
+      </label>
+      <label className={styles.labelclass}>
+        Number
+        <input
+          className={styles.inputclass}
+          type="tel"
+          name="number"
+          title="Phone number must be digits and can contain spaces, dashes or parentheses."
+          required
+          value={number}
+          onChange={handleChange}
+          ref={numberInputRef}
+          placeholder="Ex: 123-44-56"
+        />
+      </label>
+      {error && <p className={styles.errorMessage}>{error}</p>}
+
+      <button type="submit" className={styles.buttonclass}>
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+ContactForm.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import App from './components/App';
+import './index.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ReduxProvider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </ReduxProvider>
+);
